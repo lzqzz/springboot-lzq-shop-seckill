f31001b03b29b4523f3961e71c526af491776c20
lzzqq <1642870969@qq.com>
2022-04-15 17:51:57 +0800

    create project

---
+++ src/main/java/com/lzq/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.lzq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String test(){
+        return "fail";
+    }
+}
